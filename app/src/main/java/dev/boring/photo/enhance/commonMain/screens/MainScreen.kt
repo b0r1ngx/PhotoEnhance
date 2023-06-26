fadfe592647f1e7b75a8f46dc8b4e1efa5bd3898
Kirill I. A <ghztrikz1337@gmail.com>
2023-06-26 10:18:09 +0300

    1. Solve TODO: `Use colors of MaterialTheme`

--- app/src/main/java/dev/boring/photo/enhance/commonMain/screens/MainScreen.kt
+++ app/src/main/java/dev/boring/photo/enhance/commonMain/screens/MainScreen.kt
@@ -20,13 +20,13 @@ import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
@@ -143,7 +143,7 @@ private fun LineBetweenPictures(
             .width(dividerWidth)
             .fillMaxHeight()
             .offset { IntOffset((progress * 3).roundToInt(), 0) },
-        color = Color.White // TODO: Use colors of MaterialTheme
+        color = MaterialTheme.colorScheme.onSecondary
     )
 }
 
